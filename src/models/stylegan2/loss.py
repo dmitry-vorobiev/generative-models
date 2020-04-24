6922aa2797e813d873a2f2ee07c194bb719491fd
@dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-04-24 16:28:06 +0300

    loss modules contain G and D modules

--- src/models/stylegan2/loss.py
+++ src/models/stylegan2/loss.py
@@ -5,9 +5,11 @@ import torch.nn.functional as F
 from torch import autograd, nn, Tensor
 from typing import Union, Tuple
 
+from .net import Discriminator, Generator, Latent, Label
+
 
 def r1_penalty(real_pred: Tensor, real_img: Tensor):
-    grad, = autograd.grad(real_pred.sum(), real_img, retain_graph=True)
+    grad, = autograd.grad(real_pred.sum(), real_img, create_graph=True)
     return grad.pow(2).sum(dim=(1, 2, 3))[:, None]
 
 
@@ -16,29 +18,46 @@ class D_LogisticLoss_R1(nn.Module):
     """ R1 and R2 regularizers from the paper
         "Which Training Methods for GANs do actually Converge?", Mescheder et al. 2018
     """
-    def __init__(self, r1_freq=16, r1_gamma=10.0):
+
+    def __init__(self, generator, discriminator, r1_freq=16, r1_gamma=10.0):
+        # type: (Generator, Discriminator, int, float) -> D_LogisticLoss_R1
         super(D_LogisticLoss_R1, self).__init__()
+        self.generator = generator
+        self.discriminator = discriminator
         self.freq = r1_freq
         self.gamma = r1_gamma
-
         self.count = 0
 
-    def forward(self, real_pred: Tensor, fake_pred: Tensor, real_img: Tensor) -> Tensor:
-        loss = F.softplus(fake_pred) + F.softplus(-real_pred)
+    @property
+    def should_reg(self) -> bool:
+        return self.count % self.freq == 0
 
-        if self.count % self.freq == 0:
-            penalty = r1_penalty(real_pred, real_img)
+    def update_count(self) -> None:
+        self.count = (self.count + 1) % self.freq
+
+    def forward(self, reals, z, label=None):
+        # type: (Tensor, Latent, Label) -> Tensor
+        if self.should_reg:
+            reals.requires_grad_(True)
+
+        fakes, fake_w = self.generator(z, label)
+        real_score = self.discriminator(reals, label)
+        fake_score = self.discriminator(fakes, label)
+        loss = F.softplus(fake_score) + F.softplus(-real_score)
+
+        if self.should_reg:
+            penalty = r1_penalty(real_score, reals)
             reg = penalty * (self.gamma * 0.5)
             loss = loss + (reg * self.freq)
 
-        self.count = (self.count + 1) % self.freq
+        self.update_count()
         return loss
 
 
 def path_length(fake_img: Tensor, fake_w: Tensor) -> Tensor:
     N, C, H, W = fake_img.shape
     noise = torch.randn_like(fake_img) / math.sqrt(H * W)
-    grad, = autograd.grad((fake_img * noise).sum(), fake_w, retain_graph=True)
+    grad, = autograd.grad((fake_img * noise).sum(), fake_w, create_graph=True)
     # fake_w: (L, N, S)
     return torch.sqrt(grad.pow(2).sum(dim=2, keepdim=True).mean(dim=0))
 
@@ -54,24 +73,38 @@ def path_len_penalty(fake_img, fake_w, path_len_avg=0.0, decay=0.01):
 
 # noinspection PyPep8Naming
 class G_LogisticNSLoss_PathLenReg(nn.Module):
-    """ Non-saturating logistic loss with path length regularizer from the paper
-        "Analyzing and Improving the Image Quality of StyleGAN", Karras et al. 2019
+    """Non-saturating logistic loss with path length regularizer from the paper
+       "Analyzing and Improving the Image Quality of StyleGAN", Karras et al. 2019
     """
-    def __init__(self, pl_ema_decay=0.01, pl_reg_freq=4, pl_reg_weight=2.0):
+
+    def __init__(self, generator, discriminator,
+                 pl_ema_decay=0.01, pl_reg_freq=4, pl_reg_weight=2.0):
+        # type: (Generator, Discriminator, float, int, float) -> G_LogisticNSLoss_PathLenReg
         super(G_LogisticNSLoss_PathLenReg, self).__init__()
+        self.generator = generator
+        self.discriminator = discriminator
         self.decay = pl_ema_decay
         self.freq = pl_reg_freq
         self.weight = pl_reg_weight
 
-        self.register_buffer('path_len_avg', torch.zeros(1))
+        self.register_buffer('pl_avg', torch.zeros(1))
         self.count = 0
 
-    def forward(self, fake_pred: Tensor, fake_img: Tensor, fake_w: Tensor) -> Tensor:
-        loss = F.softplus(-fake_pred)
+    @property
+    def should_reg(self) -> bool:
+        return self.count % self.freq == 0
 
-        if self.count % self.freq == 0:
-            penalty, self.path_len_avg = path_len_penalty(
-                fake_img, fake_w, self.path_len_avg, self.decay)
+    def update_count(self) -> None:
+        self.count = (self.count + 1) % self.freq
+
+    def forward(self, z, label=None):
+        # type: (Latent, Label) -> Tensor
+        fakes, w = self.generator(z, label)
+        fake_score = self.discriminator(fakes, label)
+        loss = F.softplus(-fake_score)
+
+        if self.should_reg:
+            penalty, self.pl_avg = path_len_penalty(fakes, w, self.pl_avg, self.decay)
 
             # Note: The division in pl_noise decreases the weight by num_pixels,
             # and the reduce_mean in pl_lengths decreases it by num_affine_layers.
@@ -85,5 +118,5 @@ class G_LogisticNSLoss_PathLenReg(nn.Module):
             reg = penalty * self.weight
             loss = loss + (reg * self.freq)
 
-        self.count = (self.count + 1) % self.freq
+        self.update_count()
         return loss
