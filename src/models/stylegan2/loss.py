afe662e74a11ef7ce29ac181e01444876495fdbb
@dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-04-24 11:21:32 +0300

    stylegan2 default losses

---
+++ src/models/stylegan2/loss.py
@@ -0,0 +1,90 @@
+import math
+import torch
+import torch.nn.functional as F
+
+from torch import autograd, nn, Tensor
+from typing import Union, Tuple
+
+
+def r1_penalty(real_pred: Tensor, real_img: Tensor):
+    grad, = autograd.grad(real_pred.sum(), real_img, retain_graph=True)
+    return grad.pow(2).sum(dim=(1, 2, 3))[:, None]
+
+
+# noinspection PyPep8Naming
+class D_LogisticLoss_R1(nn.Module):
+    """ R1 and R2 regularizers from the paper
+        "Which Training Methods for GANs do actually Converge?", Mescheder et al. 2018
+    """
+    def __init__(self, r1_freq=16, r1_gamma=10.0):
+        super(D_LogisticLoss_R1, self).__init__()
+        self.freq = r1_freq
+        self.gamma = r1_gamma
+
+        self.count = 0
+
+    def forward(self, real_pred: Tensor, fake_pred: Tensor, real_img: Tensor) -> Tensor:
+        loss = F.softplus(fake_pred) + F.softplus(-real_pred)
+
+        if self.count % self.freq == 0:
+            penalty = r1_penalty(real_pred, real_img)
+            reg = penalty * (self.gamma * 0.5)
+            loss = loss + (reg * self.freq)
+
+        self.count = (self.count + 1) % self.freq
+        return loss
+
+
+def path_length(fake_img: Tensor, fake_w: Tensor) -> Tensor:
+    N, C, H, W = fake_img.shape
+    noise = torch.randn_like(fake_img) / math.sqrt(H * W)
+    grad, = torch.autograd.grad(
+        (fake_img * noise).sum(), fake_w, retain_graph=True)
+    # fake_w: (L, N, S)
+    return torch.sqrt(grad.pow(2).sum(dim=2, keepdim=True).mean(dim=0))
+
+
+def path_len_penalty(fake_img, fake_w, path_len_avg=0.0, decay=0.01):
+    # type: (Tensor, Tensor, Union[float, Tensor], float) -> Tuple[Tensor, Tensor]
+    path_len = path_length(fake_img, fake_w)
+    with torch.no_grad():
+        path_len_avg = path_len_avg + decay * (path_len.mean() - path_len_avg)
+    penalty = torch.pow(path_len - path_len_avg, 2)
+    return penalty, path_len_avg
+
+
+# noinspection PyPep8Naming
+class G_LogisticNSLoss_PathLenReg(nn.Module):
+    """ Non-saturating logistic loss with path length regularizer from the paper
+        "Analyzing and Improving the Image Quality of StyleGAN", Karras et al. 2019
+    """
+    def __init__(self, pl_ema_decay=0.01, pl_reg_freq=4, pl_reg_weight=2.0):
+        super(G_LogisticNSLoss_PathLenReg, self).__init__()
+        self.decay = pl_ema_decay
+        self.freq = pl_reg_freq
+        self.weight = pl_reg_weight
+
+        self.register_buffer('path_len_avg', torch.zeros(1))
+        self.count = 0
+
+    def forward(self, fake_pred: Tensor, fake_img: Tensor, fake_w: Tensor) -> Tensor:
+        loss = F.softplus(-fake_pred)
+
+        if self.count % self.freq == 0:
+            penalty, self.path_len_avg = path_len_penalty(
+                fake_img, fake_w, self.path_len_avg, self.decay)
+
+            # Note: The division in pl_noise decreases the weight by num_pixels,
+            # and the reduce_mean in pl_lengths decreases it by num_affine_layers.
+            # The effective weight then becomes:
+            #
+            # gamma_pl = pl_weight / num_pixels / num_affine_layers
+            # = 2 / (r^2) / (log2(r) * 2 - 2)
+            # = 1 / (r^2 * (log2(r) - 1))
+            # = ln(2) / (r^2 * (ln(r) - ln(2))
+            #
+            reg = penalty * self.weight
+            loss = loss + (reg * self.freq)
+
+        self.count = (self.count + 1) % self.freq
+        return loss
