812f7e66f61027c22d7b87653013e9b7491ea402
@dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-05-20 13:04:45 +0300

    add pl_minibatch_shrink for pl_reg (+speed, +batch_size)

--- src/models/stylegan2/loss.py
+++ src/models/stylegan2/loss.py
@@ -1,3 +1,4 @@
+import logging
 import math
 import torch
 import torch.nn.functional as F
@@ -7,6 +8,9 @@ from typing import Optional, Union, Tuple
 
 from .net import Discriminator, Generator
 from my_types import FloatDict, LossWithStats
+
+
+log = logging.getLogger(__name__)
 
 
 def r1_penalty(real_pred: Tensor, real_img: Tensor, reduce_mean=True) -> Tensor:
@@ -70,24 +74,24 @@ class D_LogisticLoss_R1(nn.Module):
         return loss, stats
 
 
-def path_length(fake_img: Tensor, fake_w: Tensor) -> Tensor:
-    N, C, H, W = fake_img.shape
+def path_length(fake_images: Tensor, fake_w: Tensor) -> Tensor:
+    N, C, H, W = fake_images.shape
     # Compute |J*y|.
-    pl_noise = torch.randn_like(fake_img).div_(math.sqrt(H * W))
-    pl_grad, = autograd.grad(torch.sum(fake_img * pl_noise), fake_w, create_graph=True)
+    pl_noise = torch.randn_like(fake_images).div_(math.sqrt(H * W))
+    pl_grad, = autograd.grad(torch.sum(fake_images * pl_noise), fake_w, create_graph=True)
     # fake_w comes in shape (L, N, S).
     # Summing style_dim (dlatent) and averaging across all layers.
     return torch.sqrt(pl_grad.pow(2).sum(dim=2, keepdim=True).mean(dim=0))
 
 
-def path_len_penalty(fake_img, fake_w, pl_mean=0.0, pl_decay=0.01, reduce_mean=True):
+def path_len_penalty(fake_images, fake_w, pl_avg=0.0, pl_decay=0.01, reduce_mean=True):
     # type: (Tensor, Tensor, Union[float, Tensor], float, bool) -> Tensor
-    pl_lengths = path_length(fake_img, fake_w)
+    pl_lengths = path_length(fake_images, fake_w)
     # Track exponential moving average of |J*y|. It's updated inplace.
     with torch.no_grad():
-        pl_mean += pl_decay * (pl_lengths.mean() - pl_mean)
+        pl_avg += pl_decay * (pl_lengths.mean() - pl_avg)
     # Calculate (|J*y|-a)^2.
-    penalty = (pl_lengths - pl_mean) ** 2
+    penalty = torch.pow(pl_lengths - pl_avg, 2)
     if reduce_mean:
         penalty = penalty.mean()
     return penalty
@@ -95,21 +99,47 @@ def path_len_penalty(fake_img, fake_w, pl_mean=0.0, pl_decay=0.01, reduce_mean=T
 
 # noinspection PyPep8Naming
 class G_LogisticNSLoss_PathLenReg(nn.Module):
-    """Non-saturating logistic loss with path length regularizer from the paper
+    r"""Non-saturating logistic loss with path length regularizer from the paper
        "Analyzing and Improving the Image Quality of StyleGAN", Karras et al. 2019
-    """
 
-    def __init__(self, pl_ema_decay=0.01, pl_reg_interval=4, pl_reg_weight=2.0):
+       Args:
+            pl_decay: Decay for tracking the moving average of path lengths.
+            pl_reg_weight: Scale for regularization term.
+            pl_reg_interval: Number of training steps between consecutive regularized updates,
+                None - disable lazy regularization
+            pl_minibatch_shrink: Use lower batch_size to evaluate regularization term,
+                None - use original batch_size
+    """
+    def __init__(self, pl_decay=0.01, pl_reg_weight=2.0, pl_reg_interval=4,
+                 pl_minibatch_shrink=2):
         super(G_LogisticNSLoss_PathLenReg, self).__init__()
-        self.decay = pl_ema_decay
-        self.reg_interval = pl_reg_interval
+
+        if pl_decay <= 0.0:
+            raise AttributeError("pl_decay should be greater than zero")
+
+        if pl_reg_weight is None or pl_reg_weight <= 0.0:
+            log.warning("Given pl_reg_weight={} path length regularization will be turned off"
+                        .format(pl_reg_weight))
+            pl_reg_weight = 0.0
+
+        if pl_reg_interval is None or pl_reg_interval < 1:
+            pl_reg_interval = 1
+
+        if pl_minibatch_shrink is None or pl_minibatch_shrink < 1:
+            pl_minibatch_shrink = 1
+
+        self.pl_decay = pl_decay
         self.pl_weight = pl_reg_weight
+        self.reg_interval = pl_reg_interval
+        self.pl_minibatch_shrink = pl_minibatch_shrink
 
         self.register_buffer('pl_avg', torch.zeros(1))
         self.count = 0
 
     @property
     def should_reg(self) -> bool:
+        if self.pl_weight <= 0.0:
+            return False
         return self.count % self.reg_interval == 0
 
     def update_count(self) -> None:
@@ -133,7 +163,18 @@ class G_LogisticNSLoss_PathLenReg(nn.Module):
         del fake_score
 
         if self.should_reg:
-            penalty = path_len_penalty(fakes, w, self.pl_avg, self.decay, reduce_mean=True)
+
+            # Evaluate the regularization term using a smaller minibatch to conserve memory.
+            if self.pl_minibatch_shrink > 1:
+                N = fakes.size(0)
+                N_pl = max(N // self.pl_minibatch_shrink, 1)
+                indices = torch.randperm(N, device=z.device)[:N_pl]
+                z = z[indices]
+                if label is not None:
+                    label = label[indices]
+                fakes, w = G(z, label)
+
+            penalty = path_len_penalty(fakes, w, self.pl_avg, self.pl_decay, reduce_mean=True)
             stats['G_pl'] = penalty.item()
 
             # Apply weight.
