8b331aa4017f580158dbcb9990d83347c3ead56c
@dmitry-vorobiev <25718561+dmitry-vorobiev@users.noreply.github.com>
2020-06-26 13:36:58 +0300

    SG2 projector: fix huge noise_reg magnitude

--- src/models/stylegan2/project.py
+++ src/models/stylegan2/project.py
@@ -12,9 +12,9 @@ log = logging.getLogger(__name__)
 
 
 def preproc_image(image: Tensor) -> Tensor:
+    image = image.clamp(-1, 1)
     # convert image to pixel range
     # (-1, 1) -> (0, 2) -> (0, 255)
-    # maybe clip to (-1, 1) ?
     image = (image + 1) * (255 / 2)
 
     H, W = image.shape[2:]
@@ -26,55 +26,55 @@ def preproc_image(image: Tensor) -> Tensor:
 
 
 class Projector(object):
-    def __init__(self, G: Generator, batch_size=1, num_steps=1000, dlatent_avg_samples=10_000,
-                 init_lr=0.1, lr_warm_len=0.05, lr_anneal_len=0.25, init_noise_mult=0.05,
+    def __init__(self, G: Generator, batch_size=1, num_steps=1000, w_avg_samples=10_000,
+                 lr=0.1, lr_warm_len=0.05, lr_anneal_len=0.25, noise_mult=0.05,
                  noise_ramp_len=0.75, reg_noise_weight=1e5, device=None):
         self.batch_size = batch_size
         self.G = G.to(device)
         self.num_steps = num_steps
-        self.dlatent_avg_samples = dlatent_avg_samples
-        self.init_lr = init_lr
-        self.init_noise_mult = init_noise_mult
+        self.lr = lr
         self.lr_warm_len = lr_warm_len
         self.lr_anneal_len = lr_anneal_len
+        self.noise_mult = noise_mult
         self.noise_ramp_len = noise_ramp_len
         self.reg_noise_weight = reg_noise_weight
         self.device = device
 
-        # maybe use nn.Module for this?
-        w = torch.zeros(batch_size, *G.w_avg.shape).to(device)
-        self.w = nn.Parameter(w, requires_grad=True)
         self.distance = PNetLin().to(device)
 
         # call self.init() first
+        self.w = None
         self.noise_params = None
         self.target_image = None
         self.optim = None
         self._cur_step = 0
 
         if G is None:
-            raise AttributeError("Unable initialize projector with G=None")
+            raise AttributeError("Unable to init projector without G")
 
         if G.synthesis.randomize_noise:
             raise AttributeError("randomize_noise in G is unsupported")
 
+        G.truncation_psi = None
+        G.eval()
+        G.requires_grad_(False)
+
         # Find dlatent stats.
-        print("Searching W mean and std using %d samples" % dlatent_avg_samples)
+        print("Searching W mean and std using %d samples" % w_avg_samples)
         torch.manual_seed(123)
-        z = torch.randn(dlatent_avg_samples, G.latent_dim, device=device)
-        w: Tensor = G.mapping(z, label=None)
+        z = torch.randn(w_avg_samples, G.latent_dim, device=device)
+        w = G.mapping(z, label=None)
         w_mean = w.mean(dim=0, keepdim=True)
-        self.w_std = torch.sqrt((w - w_mean).pow_(2).sum() / dlatent_avg_samples)
-        print("W std: %g" % self.w_std)
+        w_std = torch.sqrt(torch.sum((w - w_mean) ** 2) / w_avg_samples)
+        print("W std: %g" % w_std)
+
+        self.w_mean = w_mean
+        self.w_std = w_std
 
     def init(self, image: Tensor):
         print("Preparing target images...")
         self.target_image = preproc_image(image.to(self.device))
         G = self.G
-
-        G.truncation_psi = None
-        G.eval()
-        G.requires_grad_(False)
 
         # Find noise inputs.
         print("Setting up noise inputs...")
@@ -86,8 +86,9 @@ class Projector(object):
             v = buffers[name].normal_().requires_grad_(True)
             noise_params.append(v)
 
-        self.optim = torch.optim.Adam([self.w, *noise_params])
+        self.w = self.w_mean.repeat(self.batch_size, 1).requires_grad_(True)
         self.noise_params = noise_params
+        self.optim = torch.optim.Adam([self.w, *noise_params])
         self._cur_step = 0
 
     def _update_lr(self):
@@ -95,7 +96,7 @@ class Projector(object):
         lr_ramp = min(1.0, (1.0 - t) / self.lr_anneal_len)
         lr_ramp = 0.5 - 0.5 * math.cos(lr_ramp * math.pi)
         lr_ramp = lr_ramp * min(1.0, t / self.lr_warm_len)
-        lr = self.init_lr * lr_ramp
+        lr = self.lr * lr_ramp
 
         for group in self.optim.param_groups:
             group['lr'] = lr
@@ -103,7 +104,7 @@ class Projector(object):
     @property
     def _noise_mult(self):
         t = self._cur_step / self.num_steps
-        return self.w_std * self.init_noise_mult * max(0.0, 1 - t / self.noise_ramp_len) ** 2
+        return self.w_std * self.noise_mult * max(0.0, 1 - t / self.noise_ramp_len) ** 2
 
     def step(self):
         G = self.G
@@ -125,7 +126,7 @@ class Projector(object):
         self._normalize_noise()
 
         if not self._cur_step % 10:
-            print("it: {}, loss: {:.3f}, reg: {:.3f}, dist: {:.7f}".format(
+            print("it: {}, loss: {:.3f}, reg: {:.7f}, dist: {:.7f}".format(
                 self._cur_step, loss.item(), reg, dist.mean().item()))
         self._cur_step += 1
 
@@ -139,7 +140,7 @@ class Projector(object):
                 if size <= 8:
                     break
                 # Downscale
-                p = F.interpolate(p, scale_factor=0.5, mode="nearest")
+                p = F.avg_pool2d(p, 2)
                 size = size // 2
 
         return reg
@@ -147,4 +148,6 @@ class Projector(object):
     def _normalize_noise(self):
         with torch.no_grad():
             for p in self.noise_params:
-                p.add_(-1, p.mean()).div_(p.std())
+                mean = p.mean()
+                std = torch.mean((p - mean) ** 2).sqrt()  # p.std() ?
+                p.add_(-mean).div_(std)
